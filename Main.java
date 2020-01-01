f6189e86293785898d571325b336267cf4072784
Austin Shalit <austinshalit@gmail.com>
2020-01-01 15:41:16 -0800

    Update to show ld and dyld failure

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    String v = System.getenv("TestVar");
-    System.out.println(v);
-    
-    ;
+    System.out.println(System.getenv("LD_LIBRARY_PATH"));
+    System.out.println(System.getenv("DYLD_LIBRARY_PATH"));
+    System.out.println(System.getenv("TestVar"));
   }
 }
